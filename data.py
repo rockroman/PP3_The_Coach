d7a16487d5c29e09af35e16ef099a4686d1d354e
rockroman <rock.rakic@gmail.com>
2022-10-10 16:37:35 +0000

    Create file for Data manipulation and import pygsheets module

---
+++ data.py
@@ -0,0 +1,7 @@
+"""
+A simple, intuitive python library to access google spreadsheets
+through the Google Sheets API v4
+"""
+import pygsheets
+
+PATH = 'C:\\Users\\una&roman\\OneDrive\\Radna površina\\test.py\\creds.json'
