cf620bd0d580d4bb833d6312915674e5ee79d349
rockroman <rock.rakic@gmail.com>
2022-10-11 14:18:18 +0000

    Create authentication file to authenticate and validate user

--- user_authentication.py
+++ user_authentication.py
@@ -1,36 +1,38 @@
-# from data import sh
-# import re
+from data import sh
+import re
+from email_validator import validate_email, EmailNotValidError
 
-# WK2 = sh[1]
-# USER_DATA = []
+WK2 = sh[1]
+USER_DATA = []
 
  
-# def display_menu():
-#     status = input("Hello are you a New user? y/n? \n")
-#     if status == "y":
-#         old_user()
-#     elif status == "n":
-#         new_user()
+def display_menu():
+    print("Hello and Welcome are you a New user?")
+    status = input("press 'y' for yes and 'n' for no \n")
+    if status == "y":
+        # old_user()
+        pass
+    elif status == "n":
+        new_user()
  
 
-# def new_user():
-#     while True:
-#         username = input("PLease enter your username: \n ")
-#         if not re.match(r'^[a-zA-z0-9]{2,12}$', username):
-#             print(
-#                     'userame must be 2 to 12 characters long and contain'
-#                     'letters and numbers')
-#             continue
-#         break
-#     while True:
-#         email = input("Please enter your email address: \n ")
-#         if not re.match(r'^[\w.-]+@[\w.-]{3,15}$', email):
-#             print('email must be 3 to 15 characters long')  
-#             continue
-#         break
+def new_user():
+    while True:
+        username = input("Please enter your username: \n ")
+        if not re.match(r'^[a-zA-z0-9]{2,12}$', username):
+            print(
+                    'userame must be 2 to 12 characters long and contain'
+                    'letters and numbers')
+            continue
+        break
+    while True:
+        email = input("Please enter your email address: \n ")
+        
+            continue
+        break
 
 
-
+display_menu()
 # try
 
 
@@ -72,4 +74,3 @@
 #         print("\nUser doesn't exist or wrong password!\n")
  
 # while status != "q":
-#     displayMenu()
