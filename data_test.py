4aea0404475673abf6ae137ba5cec26404c4b3bc
rockroman <rock.rakic@gmail.com>
2022-10-25 22:49:40 +0000

    Uncomment unittest code and clear white space

--- data_test.py
+++ data_test.py
@@ -7,10 +7,10 @@ class TestUserScore(unittest.TestCase):
     test user score function
     with inputs
     """
-    # def test_user_score_valid(self):
-    #     self.assertEqual(user_score(0), 0)
-    #     self.assertEqual(user_score(23), 23)
-    #     self.assertEqual(user_score(85.8), 85.8)
+    def test_user_score_valid(self):
+        self.assertEqual(user_score(0), 0)
+        self.assertEqual(user_score(23), 23)
+        self.assertEqual(user_score(85.8), 85.8)
 
     def test_user_score_type(self):
         val = ""
@@ -20,7 +20,3 @@ class TestUserScore(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
